3a2eae42a62483b32c9c4aa5672532baf0673985
Josh Seipel <joshseipel@gmail.com>
2017-03-07 18:25:13 -0500

    1. Prompt user to play game

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+console.log("We're all good hear!")
+
+var startButton = document.getElementById('yesButton');
+var startButton = document.getElementById('noButton');
+
+}
+
+function myFunction() {
+    document.getElementById("yesButton");
+    prompt("Hello, would you like to play Trivia Game");
+}
+
+
+
+console.log("This is working!")
