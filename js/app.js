30e46a7dec0bafc6d9f303ca94da5f183c328234
Josh Seipel <joshseipel@gmail.com>
2017-03-08 13:22:31 -0500

    4. Uploaded gameBoard func

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ window.onload = function() {
 
 console.log("We're all good here!")
 }
-function myFunction() {
-    var promptAnswer = prompt("Hello, would you like to play Trivia Game, please input 'Yes' or 'No' ");
+function acknowledgeUser () {
+    var promptAnswer = prompt("Hello, would you like to play Trivia Game, please input 'Yes' or 'No'. ");
     	promptAnswer = promptAnswer.toUpperCase();
     	if (promptAnswer === 'YES') {
     	   userName();
       } else if (promptAnswer === 'NO') {
-    	   alert('Goodbye');//Done.
+    	   alert('See you next time!');//Done.
       } else {
 
     }
@@ -18,7 +18,7 @@ var userName = function(x){//Done.
 	var promptUserName = prompt("What is your name?");
 	promptUserName = promptUserName.toUpperCase();
 	 if (promptUserName !== '') {
-	 	alert('Hello, ' + promptUserName);
+	 	alert('Hello, ' + promptUserName + '!');
     	startGame();
 	 } else {
 	    alert('Thanks for stopping by!');
@@ -26,9 +26,38 @@ var userName = function(x){//Done.
 };
 
 function startGame(){
-	alert('Game has started!'); //Start Game logic here.
-}
+	// making question/answer boxes
+	for ( var i = 0; i < 6; i++) {
+	// horizontal container for Q&A
+	var container = document.createElement('div');
+	container.className = 'container';
+	// question embedded in container
+	var question = document.createElement('div');
+	question.className = 'question';
+	container.appendChild(question);
+
+
+
+	for (var j=0; j < 5; j++) {
+		var answer = document.createElement('div');
+		answer.className = 'answer';
+		question.appendChild(answer);
+
+		}
+		// Last line of first for loop is appending
+		document.getElementById('child').append(container);
+	}
+};
+
+createGame();
+
+console.log("This works with John's assistence.")
+
+
+
+
+alert('Game has started!'); //Start Game logic here.
 
 console.log("This is working!");
 
- //This is the end to window.onload.//
+
