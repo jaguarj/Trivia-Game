610c55cce783686e9d2cc9328674a9d4bf50a050
Josh Seipel <joshseipel@gmail.com>
2017-03-08 09:48:08 -0500

    2. Updated user prompt to ask to start game

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 window.onload = function() {
 
-console.log("We're all good hear!")
-
-var startButton = document.getElementById('yesButton');
-var startButton = document.getElementById('noButton');
-
+console.log("We're all good here!")
 }
-
 function myFunction() {
-    document.getElementById("yesButton");
-    prompt("Hello, would you like to play Trivia Game");
+    var promptAnswer = prompt("Hello, would you like to play Trivia Game, please input 'Yes' or 'No' ");
+    	promptAnswer = promptAnswer.toUpperCase();
+    	if (promptAnswer === 'YES') {
+
+    	   userName('josh');
+
+      } else if (promptAnswer === 'NO') {
+
+    	   alert('Goodbye');
+
+      } else {
+
+    }
+};
+
+var userName = function(inputName){//Done.
+	 if (inputName !== '') {
+	 	alert('Hello, ' + inputName);
+    	startGame();
+	 } else {
+	    alert('Thanks for stopping by!');
+	}
+};
+
+function startGame(){
+	alert('Game has started!'); //Start Game logic here.
 }
 
+console.log("This is working!");
 
-
-console.log("This is working!")
+ //This is the end to window.onload.//
